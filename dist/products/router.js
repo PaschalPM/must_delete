0328c6862710d6fa5cb719e3cd6546df869ef39f
PaschalPM <okaforpaschal018@gmail.com>
2023-09-12 02:44:16 +0100

    Adjusted

--- dist/products/router.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { allProducts } from "./views.js";
-const router = Router();
-router.route("/products").get(allProducts);
-export default router;
--- dist/users/router.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { allUsers } from "./views.js";
-const router = Router();
-router.route("/users").get(allUsers);
-export default router;
